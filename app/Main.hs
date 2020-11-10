80b226f8e5d4a9e8b0f526c969ce70c9d997b6a9
Shamil Gadelshin <shamilgadelshin@gmail.com>
2020-11-10 23:54:49 +0300

    Add timestamps.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 import Control.Monad
 import Control.Concurrent
 import Data.ByteString.Char8 as Char8
+import Data.Time.Clock.POSIX (getPOSIXTime)
 
 import Message;
 import RedisManager;
@@ -11,9 +12,12 @@ import Config
 
 main :: IO ()
 main = do
+    print =<< getTimestamp 
     Char8.putStrLn getMessage
     print =<< getConfiguration
     forkIO $ forever $ do getRedisInfo
     forever $ do updateRedis
     
 
+getTimestamp :: IO Integer 
+getTimestamp = round . (* 1000) <$> getPOSIXTime
