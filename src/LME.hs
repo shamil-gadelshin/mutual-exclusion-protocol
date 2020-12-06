7cc83daf85afcf25246c32b27f6dace0c9218506
Shamil Gadelshin <shamilgadelshin@gmail.com>
2020-12-06 23:13:45 +0300

    Add input message processing to LME

--- src/LME.hs
+++ src/LME.hs
@@ -2,7 +2,8 @@
 --   https://en.wikipedia.org/wiki/Lamport%27s_distributed_mutual_exclusion_algorithm
 
 module LME
-    ( 
+    ( new
+    , request
     ) where
 
 import Control.Concurrent.MVar
@@ -27,20 +28,35 @@ data LamportMutualExclusion = LamportMutualExclusion
 new :: String -> IO Lme
 new serverId = newMVar $ LamportMutualExclusion LTS.new serverId MQ.empty
  
-request :: IO Lme -> IO M.Message
-request lme = composeMessage Nothing lme Nothing M.Request
+request :: Lme -> IO M.Message
+request lme = composeMessage lme Nothing Nothing M.Request
 
-composeMessage :: Maybe String -> IO Lme -> Maybe Integer -> M.Type -> IO M.Message
-composeMessage sourceRequestId lmeBoxed msgTs msgType = do
-    lme <- takeMVar =<< lmeBoxed
+composeMessage :: Lme -> Maybe String -> Maybe Integer -> M.Type -> IO M.Message
+composeMessage lmeBoxed sourceRequestId msgTs msgType = do
+    lme <- takeMVar lmeBoxed
     let ltsVal = lts lme
     let lts' = maybe (LTS.touch ltsVal) (LTS.update ltsVal . LTS.create) msgTs
     let ts = LTS.peek lts'
     let lme' = LamportMutualExclusion lts' (serverId lme) (queue lme) -- change to lenses
-    putMVar <$> lmeBoxed <*> return lme'
+    putMVar lmeBoxed lme'
     uuid <- newUUID
     return $ M.Message uuid ts msgType (serverId lme) sourceRequestId
 
 
 newUUID :: IO String
 newUUID = toString <$> randomIO
+
+processInputMessages :: Lme -> M.Message -> IO (Maybe M.Message)
+processInputMessages lmeBoxed msg = do
+    case M.msgType msg of 
+        M.Request -> do
+            lme <- takeMVar lmeBoxed
+            let mqVal = queue lme
+            let queue' = MQ.insert (M.timestamp msg) msg mqVal
+            let lme' = LamportMutualExclusion (lts lme) (serverId lme) queue' -- change to lenses
+            putMVar lmeBoxed lme'
+            createMsg M.Reply (Just (M.msgId msg))
+        M.Reply   -> createMsg M.Release (M.requestId msg)
+        _         -> return Nothing 
+        where 
+            createMsg t rid = Just <$> composeMessage lmeBoxed rid (Just(M.timestamp msg)) t
