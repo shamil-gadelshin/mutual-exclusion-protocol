3fa3da744be707d0fc38e84296a7acc5a8d081bb
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-03-21 15:37:28 +0300

    Improve LTS test

--- test/LTSSpec.hs
+++ test/LTSSpec.hs
@@ -12,7 +12,9 @@ spec = do
       it "`touch` works" $ do
         LTS.touch LTS.new `shouldBe` LTS.Lts 2    
       it "`create` works" $ do
-        LTS.create 10 `shouldBe` LTS.Lts 10
+        LTS.create 10 `shouldBe` LTS.Lts 10      
+      it "`peek` works" $ do
+        LTS.peek (LTS.create 10) `shouldBe` 10
       it "`update` works" $ do
         LTS.update (LTS.Lts 10) (LTS.Lts 20) `shouldBe` LTS.Lts 21
         LTS.update (LTS.Lts 20) (LTS.Lts 10) `shouldBe` LTS.Lts 21
